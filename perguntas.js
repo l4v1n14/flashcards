57fc41393509e6153a4924f90890f4c06ff13190
l4v1n14 <00001114909063SP@al.educacao.sp.gov.br>
2024-12-02 14:13:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'mundo POP',
+    'Qual série tem personagens como Ross, Rachel, Monica, Chandler, Joey e Phoebe?',
+    'A série é Friends, lançada nos anos 90'
+)
+
+criaCartao(
+    'mundo POP',
+    'Qual boyband lançou a música "What Make You Beautiful"?',
+    'A boyband One Direction em 2010'
+)
+
+criaCartao(
+    'munso POP',
+    'Qual cantora pop se tornou a arista feminina mais rica do mundo, com um patrimônio de 1,6 bilhão de dólares?',
+    'A cantora Taylor Swift'
+)
+
+criaCartao(
+    'mundo POP',
+    'Quem é conhecida como a "Rainha do POP"?',
+    'A cantora Madonna'
+)
+
+criaCartao(
+    'BOOKTOK',
+    'Qual o casal principal de "Corte de Espinhos e Rosas"?',
+    'Feyre e Rhysand'
+)
+
+criaCartao(
+    'BOOKTOK',
+    'Qual é o livro mais famoso de Shakespare?',
+    'O livro Romeu e Julieta'
+)
